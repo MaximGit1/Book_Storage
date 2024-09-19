8089a9dc20f6e55748b5251396e419c89eac852c
MaximGit1 <allq137x@gmail.com>
2024-09-20 00:22:08 +0300

    added basic structure for book model

--- book_storage/books/admin.py
+++ book_storage/books/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, Author
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "publish", "status")
+    search_fields = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "publish"
+    exclude = ["publish"]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- book_storage/books/models.py
+++ book_storage/books/models.py
@@ -1,3 +1,44 @@
+from django.utils import timezone
+from django.urls import reverse
 from django.db import models
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Book.Status.PUBLISHED)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=115)
+    slug = models.SlugField(max_length=115)
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE, related_name="book")
+    image = models.ImageField(upload_to="books/%Y/%m/")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ["-publish"]
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book:book_detail_view", args=[self.slug])
