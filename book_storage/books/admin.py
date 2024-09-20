e36c79f3fc244a531251ea9337256b0fec4b354a
MaximGit1 <allq137x@gmail.com>
2024-09-20 14:38:06 +0300

    updated debag settings and BookAdmin fields

--- book_storage/books/admin.py
+++ book_storage/books/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import Book, Author
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "publish", "status")
-    search_fields = ("title",)
     prepopulated_fields = {"slug": ("title",)}
-    date_hierarchy = "publish"
-    exclude = ["publish"]
+    readonly_fields = ("publish", "preview_image")
+    search_fields = ("title",)
+    list_editable = ("status",)
+    fields = ("title", "slug", "author", "preview_image", "image", "status", "publish")
+
+    def preview_image(self, obj: Book):
+        return mark_safe(f'<img src="{obj.image.url}" alt="img" width=100px>')
 
 
 @admin.register(Author)
