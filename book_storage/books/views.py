fba074fea2684913303f4cdda1841f8ae6f7f16e
MaximGit1 <allq137x@gmail.com>
2024-09-20 15:22:55 +0300

    created main page view

--- book_storage/books/views.py
+++ book_storage/books/views.py
@@ -1,3 +1,10 @@
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def book_list_view(request: HttpRequest) -> HttpResponse:
+    books: QuerySet[Book] = Book.published.all()
+    data = {"books": books}
+    return render(request, "books/list.html", data)
